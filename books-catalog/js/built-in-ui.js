bbafd27cfb45ef85840a0e9f4bb5f3030a405017
Евгений Каминский <2057771@mail.ru>
2018-04-20 15:31:42 +0200

    д/3

---
+++ books-catalog/js/built-in-ui.js
@@ -0,0 +1,46 @@
+/* Данный JS код */
+
+// Регулируем видимость карточки
+function toggleCardVisible () {
+ document.getElementById('content').classList.toggle('hidden');
+ document.getElementById('card').classList.toggle('hidden');
+}
+
+
+document.getElementById('close').addEventListener('click', toggleCardVisible);
+
+document.getElementById('content').addEventListener('click', (event) => {
+    let target = null;
+    if (event.target.tagName === 'LI') {
+        target = event.target;
+    }
+    if (event.target.parentNode.tagName === 'LI') {
+        target = event.target.parentNode;
+    }
+
+    if (target) {
+      toggleCardVisible();
+      document.getElementById('card-title').innerHTML = target.dataset.title;
+      document.getElementById('card-author').innerHTML = target.dataset.author;
+      document.getElementById('card-info').innerHTML = target.dataset.info;
+      document.getElementById('card-price').innerHTML = target.dataset.price;
+    }
+});
+
+let xhrBook = new XMLHttpRequest(),
+    content = document.querySelector('#content');
+
+xhrBook.addEventListener("load", onLoad);
+xhrBook.open("GET","https://neto-api.herokuapp.com/book/");
+xhrBook.send();
+
+function onLoad() {
+  let data = JSON.parse(xhrBook.responseText);
+  let str = '';
+  
+  data.forEach(function (item) {
+    str += `<li data-title="${item.title}" data-author="${item.author.name}" data-info="${item.info}" data-price="${item.price}"><img src="${item.cover.small}"></li>` 
+  })
+  
+  content.innerHTML = str;
+};
